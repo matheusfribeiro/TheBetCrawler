ac6a48d8ba9eb7fc57250514a426ed0947a3ee14
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-08-21 21:35:03 -0300

    feat: logging into account

--- src/crawler/crawler.js
+++ src/crawler/crawler.js
@@ -2,18 +2,20 @@ const puppeteer = require('puppeteer');
 const puppeteerExtra = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const {
-  delay,
+  delay, waitAndClick, checkBox, waitAndType, getDate,
   
 } = require("./helpers/helpers");
+require('dotenv').config(); 
 
 // Register the Stealth plugin
 puppeteerExtra.use(StealthPlugin());
 
 const puppeteerOptions = {
+  
   headless: false, // Whether to run the browser in headless mode ('new') or show the browser window (false)
-  slowMo: 10, // Slows down Puppeteer operations by the specified amount of milliseconds (useful for debugging)
+  slowMo: 1, // Slows down Puppeteer operations by the specified amount of milliseconds (useful for debugging)
   devtools: false, // Whether to enable DevTools in the browser
-  defaultViewport: { width: 1200, height: 800 }, // Sets the initial page viewport. Set to `null` to use the default (800x600).
+  defaultViewport: { width: 1300, height: 900 }, // Sets the initial page viewport. Set to `null` to use the default (800x600).
   args: [
     "--start-maximized",
     "--no-sandbox",
@@ -33,7 +35,7 @@ async function theBetCrawler() {
   let context;
 
   try {
-    browser = await puppeteer.launch(puppeteerOptions);
+    browser = await puppeteerExtra.launch(puppeteerOptions);
     context = await browser.createIncognitoBrowserContext();
     page = await context.newPage();
 
@@ -41,10 +43,70 @@ async function theBetCrawler() {
     try {
       console.log("it should open the page")
       await page.goto(
-        "https://www.betpremium.io/home/events-area"
+        process.env.URL
       );
       await delay(4000)
+      await checkBox(page, '.box-image', 3000)
+      await waitAndClick(page, '.cookies-button.cookies-accept-button')
       
+    } catch (error) {
+      console.log("Error:", error)
+    }
+
+    //Section 2 - Login
+    try {
+      console.log("it should login into the account")
+      const button = await page.waitForSelector('button.mat-button', { text: 'Entrar' });
+      await button.click();
+    
+      const loginSelector = 'input.mat-input-element[formcontrolname="username"]'
+      const pwdSelector = 'input.mat-input-element[formcontrolname="password"][type="password"]'
+      const login = process.env.LOGIN
+      const pwd = process.env.LOGIN_PWD
+      await waitAndType(page, loginSelector, login)
+      await waitAndType(page, pwdSelector, pwd)
+
+      await waitAndClick(page, 'button.confirm.mat-button[type="submit"]');
+      await page.waitForSelector('button.vertical-button.mat-menu-trigger[mat-button]');
+      
+      console.log('The login was successful')
+
+      
+    } catch (error) {
+      console.log("Error on login:", error)
+    }
+
+    //Section 3 - Scraping games
+    try {
+      const team = 'queretaro'
+      await waitAndClick(page, 'mat-icon.btn-search[role="img"]')
+      await waitAndType(page, 'input.mat-autocomplete-trigger.mat-input-element[role="combobox"]', team)
+      await waitAndClick(page, '#mat-option-0');
+
+      const divElement = await page.waitForSelector('div.event-date.ng-star-inserted');
+
+      // Get the text content from the <div> element
+      const divContent = await page.evaluate(el => el.textContent, divElement);
+  
+      // Extract the date using string manipulation
+      const parts = divContent.trim().split(' ');
+      const gameDate = parts[0];
+      const gameTime = parts[1];
+
+      const currentDate = getDate()
+      console.log(`The current date is ${currentDate} and it should be equal to the game date, that is: ${gameDate} and game time: ${gameTime}`)
+
+      if (currentDate === gameDate) {
+        // Continue with your code for the case when the dates match
+        console.log('The dates match. Continue with your logic.');
+      } else {
+        // Throw an error if the dates don't match
+        throw new Error('Game date does not match current date.');
+      }
+      
+      console.log('out of the statement')
+      
+      await delay(5000)
     } catch (error) {
       
     }
--- src/crawler/helpers/helpers.js
+++ src/crawler/helpers/helpers.js
@@ -1,3 +1,53 @@
 
 
 exports.delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+exports.waitAndClick = async (page, selector) => {
+  try {
+    await page.waitForSelector(selector)
+    await page.click(selector)
+  } catch (error) {
+    console.log('Error:', error)
+  }
+}
+
+exports.checkBox = async (page, selector, timeout) => {
+  try {
+    await page.waitForSelector(selector, { timeout });
+    console.log('Selector appeared on screen.');
+    await page.keyboard.press('Escape');
+  } catch (error) {
+    if (error.name === 'TimeoutError') {
+      console.log('Selector did not appear within the timeout.');
+    } else {
+      console.error('An error occurred:', error);
+    }
+  }
+}
+
+exports.waitAndType = async (page, selector, text) => {
+  try {
+    await page.waitForSelector(selector);
+    await page.focus(selector)
+    await page.keyboard.type(text);
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
+
+exports.getDate = () => {
+  const currentDate = new Date();
+
+  const day = currentDate.getDate();
+  const month = currentDate.getMonth() + 1; // Month is 0-based, so add 1
+  const year = currentDate.getFullYear() % 100; // Get last two digits of the year
+
+  // Format numbers with leading zeros if needed
+  const formattedDay = day.toString().padStart(2, '0');
+  const formattedMonth = month.toString().padStart(2, '0');
+  const formattedYear = year.toString().padStart(2, '0');
+
+  const formattedDate = `${formattedDay}/${formattedMonth}/${formattedYear}`;
+  console.log('Current Date:', formattedDate);
+  return formattedDate
+}
