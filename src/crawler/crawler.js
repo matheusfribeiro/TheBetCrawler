ace2cd1b9b1b3305e373435600f90c88297f8d98
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-08-22 22:27:08 -0300

    feat: place single bet - home or away

--- src/crawler/crawler.js
+++ src/crawler/crawler.js
@@ -1,19 +1,24 @@
-const puppeteer = require('puppeteer');
-const puppeteerExtra = require('puppeteer-extra');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const puppeteer = require("puppeteer");
+const puppeteerExtra = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const {
-  delay, waitAndClick, checkBox, waitAndType, getDate,
-  
+  delay,
+  waitAndClick,
+  checkBox,
+  waitAndType,
+  getDate,
+  betTypeHomeOrAway,
+  waitButtonEnabledClick,
+  placeBetConfirmModal,
 } = require("./helpers/helpers");
-require('dotenv').config(); 
+require("dotenv").config();
 
 // Register the Stealth plugin
 puppeteerExtra.use(StealthPlugin());
 
 const puppeteerOptions = {
-  
   headless: false, // Whether to run the browser in headless mode ('new') or show the browser window (false)
-  slowMo: 1, // Slows down Puppeteer operations by the specified amount of milliseconds (useful for debugging)
+  //slowMo: 1, // Slows down Puppeteer operations by the specified amount of milliseconds (useful for debugging)
   devtools: false, // Whether to enable DevTools in the browser
   defaultViewport: { width: 1300, height: 900 }, // Sets the initial page viewport. Set to `null` to use the default (800x600).
   args: [
@@ -28,7 +33,6 @@ const puppeteerOptions = {
   ],
 };
 
-
 async function theBetCrawler() {
   let browser;
   let page;
@@ -41,89 +45,101 @@ async function theBetCrawler() {
 
     //Section 1 - goes to the page
     try {
-      console.log("it should open the page")
-      await page.goto(
-        process.env.URL
-      );
-      await delay(4000)
-      await checkBox(page, '.box-image', 3000)
-      await waitAndClick(page, '.cookies-button.cookies-accept-button')
-      
+      console.log("it should open the page");
+      await page.goto(process.env.URL);
+      await delay(4000);
+      await checkBox(page, ".box-image", 3000);
+      await waitAndClick(page, ".cookies-button.cookies-accept-button");
     } catch (error) {
-      console.log("Error:", error)
+      console.log("Error:", error);
     }
 
     //Section 2 - Login
     try {
-      console.log("it should login into the account")
-      const button = await page.waitForSelector('button.mat-button', { text: 'Entrar' });
+      console.log("it should login into the account");
+      const button = await page.waitForSelector("button.mat-button", {
+        text: "Entrar",
+      });
       await button.click();
-    
-      const loginSelector = 'input.mat-input-element[formcontrolname="username"]'
-      const pwdSelector = 'input.mat-input-element[formcontrolname="password"][type="password"]'
-      const login = process.env.LOGIN
-      const pwd = process.env.LOGIN_PWD
-      await waitAndType(page, loginSelector, login)
-      await waitAndType(page, pwdSelector, pwd)
+
+      const loginSelector =
+        'input.mat-input-element[formcontrolname="username"]';
+      const pwdSelector =
+        'input.mat-input-element[formcontrolname="password"][type="password"]';
+      const login = process.env.LOGIN;
+      const pwd = process.env.LOGIN_PWD;
+      await waitAndType(page, loginSelector, login);
+      await waitAndType(page, pwdSelector, pwd);
 
       await waitAndClick(page, 'button.confirm.mat-button[type="submit"]');
-      await page.waitForSelector('button.vertical-button.mat-menu-trigger[mat-button]');
-      
-      console.log('The login was successful')
+      await page.waitForSelector(
+        "button.vertical-button.mat-menu-trigger[mat-button]"
+      );
 
-      
+      console.log("The login was successful");
     } catch (error) {
-      console.log("Error on login:", error)
+      console.log("Error on login:", error);
     }
 
-    //Section 3 - Scraping games
+    //Section 3 - Scraping games and validating
     try {
-      const team = 'queretaro'
-      await waitAndClick(page, 'mat-icon.btn-search[role="img"]')
-      await waitAndType(page, 'input.mat-autocomplete-trigger.mat-input-element[role="combobox"]', team)
-      await waitAndClick(page, '#mat-option-0');
+      const team = "sc internacional";
+      
+      await waitAndClick(page, 'mat-icon.btn-search[role="img"]');
+      await waitAndType(
+        page,
+        'input.mat-autocomplete-trigger.mat-input-element[role="combobox"]',
+        team
+      );
+      await waitAndClick(page, "#mat-option-0");
 
-      const divElement = await page.waitForSelector('div.event-date.ng-star-inserted');
+      const divElement = await page.waitForSelector(
+        "div.event-date.ng-star-inserted"
+      );
 
       // Get the text content from the <div> element
-      const divContent = await page.evaluate(el => el.textContent, divElement);
-  
+      const divContent = await page.evaluate(
+        (el) => el.textContent,
+        divElement
+      );
+
       // Extract the date using string manipulation
-      const parts = divContent.trim().split(' ');
-      const gameDate = parts[0];
-      const gameTime = parts[1];
+      const parts = divContent.trim().split(" ");
+      const matchDate = parts[0];
+      const matchTime = parts[1];
 
-      const currentDate = getDate()
-      console.log(`The current date is ${currentDate} and it should be equal to the game date, that is: ${gameDate} and game time: ${gameTime}`)
+      const currentDate = getDate();
+      console.log(
+        `The current date is ${currentDate} and it should be equal to the game date, that is: ${matchDate} and game time: ${matchTime}`
+      );
 
-      if (currentDate === gameDate) {
-        // Continue with your code for the case when the dates match
-        console.log('The dates match. Continue with your logic.');
-      } else {
-        // Throw an error if the dates don't match
-        throw new Error('Game date does not match current date.');
+      // Fail fast
+      if (currentDate !== matchDate) {
+        console.log("Game date does not match current date.");
+        throw new Error("Script halted due to validation mismatch.");
       }
-      
-      console.log('out of the statement')
-      
-      await delay(5000)
-    } catch (error) {
-      
-    }
 
+      console.log("Match validation ok");
+    } catch (error) {}
 
+    //Section 4 - Placing the bet on a single game
+    await betTypeHomeOrAway(page, 'Casa')
+
+    const betFieldInput = 'input.mat-input-element[formcontrolname="value"]';
+    await waitAndType(page, betFieldInput, '3')
+    await waitButtonEnabledClick(page, 'button.bet-button.ng-star-inserted')
+    await placeBetConfirmModal(page)
+
+    await delay(5000);
 
   } catch (error) {
-    console.log("Error:", error)
+    console.log("Error:", error);
   } finally {
     if (browser) {
       console.log("Browser closed successfully.");
       await browser.close();
     }
   }
-
-
 }
 
-
-theBetCrawler()
+theBetCrawler();
--- src/crawler/helpers/helpers.js
+++ src/crawler/helpers/helpers.js
@@ -1,39 +1,37 @@
-
-
 exports.delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 exports.waitAndClick = async (page, selector) => {
   try {
-    await page.waitForSelector(selector)
-    await page.click(selector)
+    await page.waitForSelector(selector);
+    await page.click(selector);
   } catch (error) {
-    console.log('Error:', error)
+    console.log("Error:", error);
   }
-}
+};
 
 exports.checkBox = async (page, selector, timeout) => {
   try {
     await page.waitForSelector(selector, { timeout });
-    console.log('Selector appeared on screen.');
-    await page.keyboard.press('Escape');
+    console.log("Selector appeared on screen.");
+    await page.keyboard.press("Escape");
   } catch (error) {
-    if (error.name === 'TimeoutError') {
-      console.log('Selector did not appear within the timeout.');
+    if (error.name === "TimeoutError") {
+      console.log("Selector did not appear within the timeout.");
     } else {
-      console.error('An error occurred:', error);
+      console.error("An error occurred:", error);
     }
   }
-}
+};
 
 exports.waitAndType = async (page, selector, text) => {
   try {
     await page.waitForSelector(selector);
-    await page.focus(selector)
+    await page.focus(selector);
     await page.keyboard.type(text);
   } catch (error) {
-    console.error('An error occurred:', error);
+    console.error("An error occurred:", error);
   }
-}
+};
 
 exports.getDate = () => {
   const currentDate = new Date();
@@ -43,11 +41,88 @@ exports.getDate = () => {
   const year = currentDate.getFullYear() % 100; // Get last two digits of the year
 
   // Format numbers with leading zeros if needed
-  const formattedDay = day.toString().padStart(2, '0');
-  const formattedMonth = month.toString().padStart(2, '0');
-  const formattedYear = year.toString().padStart(2, '0');
+  const formattedDay = day.toString().padStart(2, "0");
+  const formattedMonth = month.toString().padStart(2, "0");
+  const formattedYear = year.toString().padStart(2, "0");
 
   const formattedDate = `${formattedDay}/${formattedMonth}/${formattedYear}`;
-  console.log('Current Date:', formattedDate);
-  return formattedDate
+  console.log("Current Date:", formattedDate);
+  return formattedDate;
+};
+
+exports.betTypeHomeOrAway = async (page, homeOrAway) => {
+  try {
+    // choose bet field
+    const oddButtonSelector = "app-odd-button-plus2";
+
+    await page.waitForSelector(oddButtonSelector);
+    const oddButtons = await page.$$(oddButtonSelector);
+
+    for (const oddButton of oddButtons) {
+      const hasMatchingTitle = await page.evaluate(
+        (button, homeOrAway) => {
+          const titleElement = button.querySelector(".title");
+          return (
+            titleElement && titleElement.textContent.trim() === homeOrAway
+          );
+        },
+        oddButton,
+        homeOrAway
+      );
+
+      if (hasMatchingTitle) {
+        await oddButton.click();
+        console.log("Clicked on the button with matching title:", homeOrAway);
+        break; // Break out of the loop after clicking one button
+      }
+    }
+
+  } catch (error) {
+    console.log("error on placing the bet:", error);
+  }
+};
+
+
+exports.waitButtonEnabledClick = async (page, selector) => {
+  try {
+    
+    await page.waitForSelector(selector);
+    
+    const button = await page.$(selector);
+    const isDisabled = await button.evaluate(button => button.hasAttribute('disabled'));
+    
+    // Wait for the button to become enabled
+    while (isDisabled) {
+      await page.waitForTimeout(100); // Adjust the delay as needed
+      isDisabled = await button.evaluate(button => button.hasAttribute('disabled'));
+    }
+    
+    await button.click();
+  } catch (error) {
+    console.log('Error: Could not place bet', error)
+  }
+}
+
+exports.placeBetConfirmModal = async (page) => {
+  try {
+    const buttonSelector = 'button.confirm.mat-button.mat-button-base.ng-star-inserted';
+    const spanSelector = `${buttonSelector} span.mat-button-wrapper`;
+
+    await page.waitForSelector(spanSelector);
+
+    const span = await page.$(spanSelector);
+    const spanText = await span.evaluate(span => span.textContent.trim());
+    console.log(spanText)
+
+    if (spanText === 'Confirmar') {
+      const button = await span.evaluateHandle(span => span.closest('button'));
+      await button.click();
+      console.log('Bet placed')
+    } else {
+      console.log('The span does not have the expected text content.');
+      throw new Error('Error validating')
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
